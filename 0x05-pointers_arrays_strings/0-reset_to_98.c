8fc72a92f4d5c1b1d98050ba7c26a777ba430c85
root <gkinyoho@gmail.com>
2022-07-11 13:49:30 -0700

    Cleared white space; did again

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * reset_to_98 : chage integer value with pointer
- * @n: pointer 
- * Return: void
+ * reset_to_98-change value of integer with pointer
+ * @n:pointer
+ * retrun:void
  */
 void reset_to_98(int *n)
-{
-	*n = 98;
+{	
+	*n=98;
 }
