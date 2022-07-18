705c2c2a9c9693405e9caad41c6e5b75fc46f849
root <gkinyoho@gmail.com>
2022-07-18 15:50:09 -0700

    this is for 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+/**
+ * _memset - fill mem with constant byte
+ * @s: pointer to mem area 's'
+ * @b: constant byte to fill
+ * @n: number of bytes to fill
+ * Return: pointer to mem area 's'
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = ; i < n; i++)
+		s[i] = b;
+	return (s);
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is for more pointer and that.
