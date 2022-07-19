ff2126e705766abf16dcc560c4a1d686c96bfa40
root <gkinyoho@gmail.com>
2022-07-18 17:27:58 -0700

    corrected

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,9 +1,8 @@
 /**
- * set_string - set value of pointer to char
- * @s: double pointer 
- * @to: the char to point to
+ * set_string - sets value of pointer to char
+ * @s: double pointer
+ * @to: the char being pointed
  */
-
 void set_string(char **s, char *to)
 {
 	*s = to;
